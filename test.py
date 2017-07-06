6c4ee3310951cbaa7684eda1c2e6cf8413ad00bf
SlunTs <shanglun.tsai@gmail.com>
2017-07-06 14:36:32 +0800

    2017/7/6

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
+from tensorflow.contrib.tensorboard.plugins import projector
 import tensorflow as tf
+import os
+# Create randomly initialized embedding weights which will be trained.
+N = 10000 # Number of items (vocab size).
+D = 200 # Dimensionality of the embedding.
+LOG_DIR = '/tmp/tensorflow/test'
+embedding_var = tf.Variable(tf.random_normal([N,D]), name='word_embedding')
 
-# Creates a graph.
-a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-c = tf.matmul(a, b)
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-# Runs the op.
-print(sess.run(c))
+# Format: tensorflow/tensorboard/plugins/projector/projector_config.proto
+config = projector.ProjectorConfig()
+
+# You can add multiple embeddings. Here we add only one.
+embedding = config.embeddings.add()
+embedding.tensor_name = embedding_var.name
+# Link this tensor to its metadata file (e.g. labels).
+embedding.metadata_path = os.path.join(LOG_DIR, 'metadata.tsv')
+
+# Use the same LOG_DIR where you stored your checkpoint.
+summary_writer = tf.summary.FileWriter(LOG_DIR)
+
+# The next line writes a projector_config.pbtxt in the LOG_DIR. TensorBoard will
+# read this file during startup.
+projector.visualize_embeddings(summary_writer, config)
