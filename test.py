afc1e9e6c0526399f9c2b111abb7253514d0c62d
SlunTs <shanglun.tsai@gmail.com>
2017-07-10 17:23:32 +0800

    2017/7/10

--- test.py
+++ test.py
@@ -1,24 +1,14 @@
-from tensorflow.contrib.tensorboard.plugins import projector
-import tensorflow as tf
-import os
-# Create randomly initialized embedding weights which will be trained.
-N = 10000 # Number of items (vocab size).
-D = 200 # Dimensionality of the embedding.
-LOG_DIR = '/tmp/tensorflow/test'
-embedding_var = tf.Variable(tf.random_normal([N,D]), name='word_embedding')
-
-# Format: tensorflow/tensorboard/plugins/projector/projector_config.proto
-config = projector.ProjectorConfig()
-
-# You can add multiple embeddings. Here we add only one.
-embedding = config.embeddings.add()
-embedding.tensor_name = embedding_var.name
-# Link this tensor to its metadata file (e.g. labels).
-embedding.metadata_path = os.path.join(LOG_DIR, 'metadata.tsv')
-
-# Use the same LOG_DIR where you stored your checkpoint.
-summary_writer = tf.summary.FileWriter(LOG_DIR)
-
-# The next line writes a projector_config.pbtxt in the LOG_DIR. TensorBoard will
-# read this file during startup.
-projector.visualize_embeddings(summary_writer, config)
+import tensorflow as tf;    
+import numpy as np;    
+import matplotlib.pyplot as plt;    
+  
+v1 = tf.get_variable(name='v1', shape=[1], initializer=tf.constant_initializer(1))  
+tf.add_to_collection('loss', v1)  
+v2 = tf.get_variable(name='v2', shape=[1], initializer=tf.constant_initializer(2))  
+tf.add_to_collection('loss', v2)  
+  
+with tf.Session() as sess:  
+    sess.run(tf.initialize_all_variables())  
+    print (tf.get_collection('loss')) 
+    print (sess.run(tf.add_n(tf.get_collection('loss')))) 
+    
